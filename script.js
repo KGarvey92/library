8f9bc5e6843e45e49c5163410664c52fcf9d9da6
KGarvey92 <kgarvey92@gmail.com>
2022-11-07 11:17:05 +0800

    add data-attributes to book elements

--- script.js
+++ script.js
@@ -16,43 +16,47 @@ myLibrary.push(theHobbit);
 const harryPotterStone = new Book('Harry Potter and the Philsopher\'s Stone', 'J.K Rowling', 223, 'Read');
 myLibrary.push(harryPotterStone);
 
+const northernLights = new Book('Northern Lights', 'Philip Pullman', 399, 'Read');
+myLibrary.push(northernLights);
+
 
 
 // loops through array and displays books
 function displayBooks() {
     const table = document.querySelector('thead');
-    for (book of myLibrary) {
+    for (let i = 0; i < myLibrary.length; i++) {
         const row = document.createElement('tr');
+        row.dataset.attribute = [i];
         table.appendChild(row);
         const title = document.createElement('td');
-        title.innerText = `${book.title}`;
+        title.innerText = `${myLibrary[i].title}`;
+        title.dataset.attribute = [i];
         row.appendChild(title);
         const author = document.createElement('td');
-        author.innerText = `${book.author}`;
+        author.innerText = `${myLibrary[i].author}`;
+        author.dataset.attribute = [i];
         row.appendChild(author);
         const length = document.createElement('td');
-        length.innerText = `${book.length}`;
+        length.innerText = `${myLibrary[i].length}`;
+        length.dataset.attribute = [i];
         row.appendChild(length);
-        
         const status = document.createElement('td');
-        status.innerText = `${book.status}`;
+        status.innerText = `${myLibrary[i].status}`;
+        status.dataset.attribute = [i];
         row.appendChild(status);
-        
         const readIcon = document.createElement('img');
         readIcon.src = 'images/eye.svg';
         readIcon.alt = 'toggle read/unread';
         readIcon.classList.add('readIcon');
         status.appendChild(readIcon);
-        
         const remove = document.createElement('td');
-        row.appendChild(remove);
-        
+        remove.dataset.attribute = [i];
+        row.appendChild(remove); 
         const removeIcon = document.createElement('img');
         removeIcon.src = 'images/x-square.svg';
         removeIcon.alt = 'remove book';
         removeIcon.classList.add('removeBtn');
         remove.appendChild(removeIcon);
-
     }
 }
 
